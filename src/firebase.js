9790e10721b099429dba1276113b43c16fb8b1bb
rudradatta <44245797+rudradatta@users.noreply.github.com>
2022-05-01 22:49:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import {getFirestore} from "@firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCD30F3Qbgfs86y81s9QcannhkL3hk1QL8",
-    authDomain: "beaming-night-288405.firebaseapp.com",
-    projectId: "beaming-night-288405",
-    storageBucket: "beaming-night-288405.appspot.com",
-    messagingSenderId: "812128358212",
-    appId: "1:812128358212:web:96f0a20c4c5010c7161111",
-    measurementId: "G-G1QYRDV6W8"
+
   };
 
 
